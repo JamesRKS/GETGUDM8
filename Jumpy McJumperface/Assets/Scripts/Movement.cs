9175531743c5fbfc1f241d75ef78cbbb6db6db22
Unknown <jamesridgeh@gmail.com>
2018-09-18 10:28:32 -0600

    Fixed Sliding Issue Added New Platforms

--- Jumpy McJumperface/Assets/Scripts/Movement.cs
+++ Jumpy McJumperface/Assets/Scripts/Movement.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement: MonoBehaviour
-{
+public class Movement: MonoBehaviour{
 
     // Player Movement Variables
     public int MoveSpeed;
     public float JumpHeight;
+    private bool doubleJump;
 
     //Player grounded variables
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     private bool grounded;
+
+    //Non-Stick Player
+    private float moveVelocity;
+
 
     //Use this for initialization
     void Start()
@@ -36,17 +40,30 @@ public class Movement: MonoBehaviour
             Jump();
         }
 
+        //Double Jump code
+        if (grounded)
+            doubleJump = false;
+
+        if(Input.GetKeyDown (KeyCode.Space)&& !doubleJump && !grounded){
+            Jump();
+            doubleJump = true;
+        }
+        //Non-Stick Player
+        moveVelocity = 0f;
+
         //This code makes the character move from side to side using the A&D keys
         if (Input.GetKey(KeyCode.D))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            moveVelocity = MoveSpeed;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-                
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            moveVelocity = -MoveSpeed;    
         }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
 
     }
 
