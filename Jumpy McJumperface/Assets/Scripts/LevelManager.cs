046849b914a312b0ce7e1fd992ceae06c5662a48
Unknown <jamesridgeh@gmail.com>
2018-09-28 10:27:27 -0600

    Started adding Checkpoints

---
+++ Jumpy McJumperface/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckPoint;
+    private Rigidbody2D pc;
+
+    // Particles
+    public GameObject DeathParticle;
+    public GameObject RespawnParticle;
+
+    //Respawn Delay
+    public float RespawnDelay;
+
+
+    //Point Penalty on Death
+    public int PointPenaltyOnDeath;
+
+    // Store Gravity Value
+    private float GravityStore;
+
+
+	// Use this for initialization
+	void Start () {
+        pc = FindObjectOfType<Rigidbody2D> ();
+	}
+
+    public void RespawnPlayer(){
+        StartCoroutine("RespawnPCCo");
+    }
+
+    public IEnumerator RespawnPCCo(){
+        //Generate Death Particle
+        Instantiate(DeathParticle, pc.transform.position, pc.transform.rotation);
+        //Hide Player
+        pc.enabled = false;
+        pc.GetComponent<Renderer>().enabled = false;
+        // Gravity Reset
+        GravityStore = pc.GetComponent<Rigidbody2D>().gravityScale;
+        pc.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        pc.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        // Point Penalty
+        ScoreManager.AddPoints(-PointPenaltyOnDeath);
+        //Debug Message
+        Debug.Log("Player Respawn");
+        //Respawn Delay
+        yield return new WaitForSeconds(RespawnDelay);
+        //Gravity Restore
+        pc.GetComponent<Rigidbody2D>().gravityScale = GravityStore;
+    }
+}
