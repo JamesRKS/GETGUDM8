3316e6b6330afc55420939587d38cd9fecbdae54
Unknown <jamesridgeh@gmail.com>
2018-10-02 10:27:59 -0600

    Did thing

--- Jumpy McJumperface/Assets/Scripts/Respawn.cs
+++ Jumpy McJumperface/Assets/Scripts/Respawn.cs
@@ -1,13 +1,2 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class Respawn : MonoBehaviour {
-
-    public LevelManager LevelManager;
-
-	//Use this for initialization
-    void Start () {
-        LevelManager = find;
-    }
-	
-}
