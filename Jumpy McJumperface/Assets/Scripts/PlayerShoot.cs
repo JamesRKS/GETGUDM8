44c439b3d981ce73a9cef5683ee911fe1987d86b
Unknown <jamesridgeh@gmail.com>
2018-10-10 10:28:49 -0600

    Added Bullets

---
+++ Jumpy McJumperface/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+    public Transform FirePoint;
+    public GameObject Projectile;
+
+
+	//Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.S))
+            Instantiate(Projectile, FirePoint.position, FirePoint.rotation);
+    }
+	
+}
