72f89d0a6ac345e71880112b39d7f7b97d7d5fe4
James <jamesridgeh@gmail.com>
2018-10-30 10:29:30 -0600

    Made Camera Movement Smoother

---
+++ Jumpy McJumperface/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Movement PC;
+
+    public bool isFollowing;
+
+
+    // Camera Position offset
+    public float xOffset;
+    public float yOffset;
+
+	// Use this for initialization
+	void Start(){
+        PC = FindObjectOfType<Movement>();
+
+        isFollowing = true;
+	}
+
+	//Update is called once per frame
+	void Update(){
+        if(isFollowing){
+            transform.position = new Vector3(PC.transform.position.x + xOffset, PC.transform.position.y + yOffset, transform.position.z);
+
+        }
+	}
+
+}
